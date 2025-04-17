4a98774a89c3d0eb2358f67628f881cb48eb9274
qoepwurity <root@ehdms>
2025-04-17 13:00:21 +0900

    fianl

--- xv6/i8254.d
+++
@@ -1,2 +0,0 @@
-i8254.o: i8254.c /usr/include/stdc-predef.h i8254.h pci.h types.h defs.h \
- memlayout.h eth.h arp.h pkts_hdr.h
--- xv6/uthread3.d
+++
@@ -1,1 +0,0 @@
-uthread3.o: uthread3.c /usr/include/stdc-predef.h types.h stat.h user.h
